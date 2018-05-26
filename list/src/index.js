d76543156d3f89d7f8145f0b2d4810e217188ed1
da-matsumoto <da.mats1990@gmail.com>
2018-05-26 21:56:43 +0900

    hello world

--- list/src/index.js
+++ list/src/index.js
@@ -1,8 +1,5 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import registerServiceWorker from './registerServiceWorker';
+import Hello from './hello.js'
 
-ReactDOM.render(<App />, document.getElementById('root'));
-registerServiceWorker();
+ReactDOM.render(<Hello msg="Hello, world"/>, document.getElementById('root'));
